63c816e73bdea9903cd2bcc91e9f5aa232782502
unknown <krahul@synycssolutions.com>
2017-02-09 16:51:00 +0530

    changes

--- src/main/java/ssss/MyResource.java
+++ src/main/java/ssss/MyResource.java
@@ -1,9 +1,13 @@
 package ssss;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Cookie;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.NewCookie;
+import javax.ws.rs.core.Response;
 import java.sql.*;
 
 /**
@@ -20,18 +24,23 @@ public class MyResource {
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public User getIt() {
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response getIt() {
         User user=new User();
-        user.setFirst_name("nnnn");
+        user.setFirst_name("ssss");
+        Cookie cookie=new Cookie( "cccc","788");
+        cookie.getDomain();
         try {
           //  Class.forName("com.mysql.jdbc.Driver");
            // Connection con = DriverManager.getConnection(
                  //   "jdbc:mysql://localhost:3306/chat", "root", "truckway");
         }catch (Exception e){
             e.printStackTrace();
-            //return "exception";
+            //return "excepti
         }
-        return user;
+        return Response.ok().entity(user).cookie(new NewCookie("cookieResponse", "cookieValueInReturn")).build();
     }
 
 }
+
+
